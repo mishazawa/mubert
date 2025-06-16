00a587182ac2e359b19c20f56067698290b8fb6d
rybinfx <rybinfx@gmail.com>
2025-06-16 21:49:15 +0800

    slai fx

--- lib/shaders/stripes/fragment.glsl
+++ lib/shaders/stripes/fragment.glsl
@@ -8,7 +8,7 @@ varying float vDisplacement;
 #define FREQ 1.
 #define FRAC_SCALE 16.
 
-
+//#include<random>
 //#include<math>
 //#include<noise3>
 //#include<voronoi3>
@@ -23,7 +23,7 @@ void main() {
   vec3 background = mix(uColor1, key,  uUseColorKey);
   vec3 newColor = background;
 
-  vec3 POS = normalize(vPosition);
+  // vec3 POS = normalize(vPosition);
 
   newColor = maybeDrawLines(background, POS);
 
@@ -40,10 +40,10 @@ void main() {
 
   pattern = step(.5, pattern);
 
-  newColor = mix(background, newColor, pattern);
+  // newColor = mix(background, newColor, pattern);
 
   //#include<common_standard_props>
-  csm_Roughness = mix(uRoughness, 1. - uRoughness, pattern * uRoughnessPattern);
+  // csm_Roughness = mix(uRoughness, 1. - uRoughness, pattern * uRoughnessPattern);
 
   csm_DiffuseColor.rgba = vec4(newColor, 1.0);
 }
--- lib/shaders/utils/math.glsl
+++ lib/shaders/utils/math.glsl
@@ -19,3 +19,48 @@ float smoothMod(float axis, float amp, float rad){
     return amp * (1.0 / 2.0) - (1.0 / PI) * at;
 }
 
+
+mat4 rotation3d(vec3 axis, float angle) {
+  axis = normalize(axis);
+  float s = sin(angle);
+  float c = cos(angle);
+  float oc = 1.0 - c;
+  return mat4(
+		oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+    oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+    oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+		0.0,                                0.0,                                0.0,                                1.0
+	);
+}
+
+vec2 rotate2d(vec2 a, float angle) {
+  return vec2(cos(angle) * a.x - sin(angle) * a.y, sin(angle) * a.x + cos(angle) * a.y);
+}
+
+vec3 rotate3d(vec3 a, vec3 c) {
+  vec4 b = vec4(a, 1.0);
+  b = rotation3d(vec3(1.0, 0.0, 0.0), c.x) * b;
+  b = rotation3d(vec3(0.0, 1.0, 0.0), c.y) * b;
+  b = rotation3d(vec3(0.0, 0.0, 1.0), c.z) * b;
+  return b.xyz;
+}
+
+
+
+vec2 ctos(vec3 p) {
+  p = normalize(p);
+  float phi = atan(p.z, p.x) / PI / 2.0 + 0.5;
+  float the = acos(p.y) / PI;
+  return vec2(phi, the);
+}
+
+vec3 stoc(vec2 uv) {
+  vec3 p;
+  float phi = (uv.x - 0.5) * PI * 2.0;
+  float the = uv.y * PI;
+  p.x = sin(the)*cos(phi);
+  p.y = cos(the);
+  p.z = sin(the)*sin(phi);
+  return p;
+}
+
