1ed59aa77b6acff396f8e182834755cc0546c328
asd <macintoshkrug@gmail.com>
2025-06-17 14:42:49 +0300

    rm bump map add recalc normals

--- lib/shaders/common/standard_props.glsl
+++ lib/shaders/common/standard_props.glsl
@@ -4,11 +4,3 @@ csm_Clearcoat = uClearcoat;
 csm_ClearcoatRoughness = uClearcoatRoughness;
 csm_Iridescence = uIridescence;
 
-
-vec3 N = vNormal * 5.;
-csm_Bump = perturbNormalArb(
-  -vViewPosition,
-  N,
-  //vec2(dFdx(vDisplacement + (.1* line)), dFdy(vDisplacement+(.1* line))), 
-  vec2(dFdx(vDisplacement), dFdy(vDisplacement)), 
-  smoothstep(0., 1., dot(N, normalize(vViewPosition))));
--- lib/shaders/slai/vertex.glsl
+++ lib/shaders/slai/vertex.glsl
@@ -16,61 +16,45 @@ varying float vDisplacement;
 
 //#include<line_functions>
 
-vec3 calcNormalFromBump (in vec3 P0,
-                         in float pattern,
-                         in float t) {
+vec3 displace (in vec3 P, in float animation) {
+  vec3 mask = drawSinLines(vec3(0.0, 0.0, 0.0), generateSyncedPosition(P), animation) * DIST_AMP * uDisplacementAmplitude;
+  mask = smoothstep(0., 1., mask);
+  return P + normal * mask;
+}
 
-  float rv0 = random(uSeed+12.0);
-  float rv1 = random(uSeed+22.0);
-  float rv2 = random(uSeed+32.0);
-  float rv3 = random(uSeed+42.0);
 
-  vec3 P1 = P0 + normalize(P0) * 0.01;
+vec3 orthogonal(vec3 v) {
+  return normalize(abs(v.x) > abs(v.z) ? vec3(-v.y, v.x, 0.0)
+  : vec3(0.0, -v.z, v.y));
+}
 
-  float sim_amp = mix(rv0, 1.0, pattern);
-  sim_amp *= 0.1 + rv3 * 0.5 ;
-  float sim_freq = 0.3 + rv2 * 0.4;
+vec3 recalcNormals(vec3 newPos, float animation) {
+  float offset = 0.001;
+  vec3 tangent = orthogonal(normal);
+  vec3 bitangent = normalize(cross(normal, tangent));
+  vec3 neighbour1 = position + tangent * offset;
+  vec3 neighbour2 = position + bitangent * offset;
 
-  vec3 T = vec3(0.0, 0.0, t);
-  vec3 F0 = P0 * sim_freq;
-  vec3 F1 = P1 * sim_freq;
+  vec3 displacedNeighbour1 = displace(neighbour1, animation);
+  vec3 displacedNeighbour2 = displace(neighbour2, animation);
 
-  P0 += vec3(
-    snoise(F0 + 11.1 + T) * sim_amp,
-    snoise(F0 + 22.2 + T) * sim_amp,
-    snoise(F0 + 33.3 + T) * sim_amp
-  ) * sim_amp;
+  vec3 displacedTangent = displacedNeighbour1 - newPos;
+  vec3 displacedBitangent = displacedNeighbour2 - newPos;
 
-  P1 += vec3(
-    snoise(F1 + 11.1 + T) * sim_amp,
-    snoise(F1 + 22.2 + T) * sim_amp,
-    snoise(F1 + 33.3 + T) * sim_amp
-  ) * sim_amp;
-
-  return normalize(P1 - P0);
+  return normalize(cross(displacedTangent, displacedBitangent));
 }
 
 void main() {
   vPosition = position;
-  vNormal = normalize(normalMatrix * normal);
+  vNormal = normal;
   vUv = uv;
 
   float animation = uTime * SPEED;
   animation += sin(animation * 3.14 * 4.0) * 0.02;
 
-  vec3 reppos = generateSyncedPosition(vPosition);
-
-  vec3 pattern = drawSinLines(vec3(0.0, 0.0, 0.0), reppos, animation);
-  pattern = smoothstep(0.2, 0.8, pattern * 0.5 + 0.5);
-
-  vec3 mask = pattern * DIST_AMP * uDisplacementAmplitude;
-
-  vec3 newPosition = vPosition + normal * mask;
-  vDisplacement = mask.x;
-
-  // vec3 newNormal = calcNormalFromBump(newPosition, mask.x, animation);
-  // vNormal = normalize(mat3(projectionMatrix * modelViewMatrix) * normalMatrix  * -newNormal);
-  // csm_Normal = vNormal;
+  vec3 newPosition = displace(vPosition, animation);
+  vDisplacement = 0.;
 
   csm_Position = newPosition;
+  csm_Normal = recalcNormals(csm_Position, animation);
 }
