68cb5c2d065dac3d8f7dde7e76ed3306ab2d97c3
asd <macintoshkrug@gmail.com>
2025-06-27 20:01:17 +0300

    fixed type definition

--- src/sound.tsx
+++ src/sound.tsx
@@ -1,13 +1,15 @@
 import { useCallback } from "react";
-
+declare global {
+  interface Window {
+    dataArray: Uint8Array;
+  }
+}
 export function useSound() {
   const getFFT = useCallback(() => {
-    //@ts-ignore
     return Array.from(window.dataArray);
   }, []);
 
   const getRMS = useCallback((): [number, number] => {
-    //@ts-ignore
     var arr = window.dataArray;
     var sum = arr.reduce((acc: number, v: number) => acc + v, 0);
     var max = sum / arr.length / 255;
