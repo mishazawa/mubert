7baa25021446cfc74db20adfbd6e0a540307dcdd
asd <macintoshkrug@gmail.com>
2025-06-27 16:04:14 +0300

    added bs for audio

--- src/sound.tsx
+++ src/sound.tsx
@@ -1,45 +1,15 @@
-import { FFT_SIZE } from "@lib/constants";
-import imgUrl from "./test_sound.mp3";
-import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useCallback } from "react";
 
 export function useSound() {
-  const audio = useMemo<HTMLAudioElement>(
-    () => document.getElementById("audio")! as HTMLAudioElement,
-    []
-  );
-
-  const ctx = useMemo(() => new AudioContext(), []);
-  const source = useRef<any>(null!);
-
-  const analyser = useMemo(() => {
-    const a = ctx.createAnalyser();
-    a.fftSize = FFT_SIZE;
-    return a;
-  }, [ctx]);
-
-  const data = useRef<Uint8Array>(new Uint8Array(analyser.frequencyBinCount));
-
   const getFFT = useCallback(() => {
-    analyser.getByteFrequencyData(data.current);
-    return Array.from(data.current);
+    //@ts-ignore
+    return window.getFFT();
   }, []);
 
   const getRMS = useCallback((): [number, number] => {
-    const arr = data.current;
-    const sum = arr.reduce((acc, v) => acc + v, 0);
-    const max = sum / arr.length / 255;
-    return [max, max];
+    //@ts-ignore
+    return window.getRMS();
   }, []);
-
-  useEffect(() => {
-    audio.src = imgUrl;
-    audio.play();
-    if (!source.current) {
-      source.current = ctx.createMediaElementSource(audio);
-      source.current.connect(analyser);
-      analyser.connect(ctx.destination);
-    }
-  }, [audio]);
 
   return {
     getRMS,
