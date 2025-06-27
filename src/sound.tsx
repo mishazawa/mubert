e0ac5794924c3629bf07f94fdda50275bcf12a33
asd <macintoshkrug@gmail.com>
2025-06-27 19:57:47 +0300

    fix?) safari autoplay issue

--- src/sound.tsx
+++ src/sound.tsx
@@ -3,12 +3,15 @@ import { useCallback } from "react";
 export function useSound() {
   const getFFT = useCallback(() => {
     //@ts-ignore
-    return window.getFFT();
+    return Array.from(window.dataArray);
   }, []);
 
   const getRMS = useCallback((): [number, number] => {
     //@ts-ignore
-    return window.getRMS();
+    var arr = window.dataArray;
+    var sum = arr.reduce((acc: number, v: number) => acc + v, 0);
+    var max = sum / arr.length / 255;
+    return [max, max];
   }, []);
 
   return {
