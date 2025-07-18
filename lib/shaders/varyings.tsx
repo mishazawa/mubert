bd79a5d60c9ad111bbb3b5f83de8bae6d2602f43
asd <macintoshkrug@gmail.com>
2025-07-18 03:07:09 +0300

    fix err on points style

--- lib/shaders/varyings.tsx
+++ lib/shaders/varyings.tsx
@@ -22,6 +22,8 @@ varying vec3 vPosition;
 varying vec3 vPositionD;
 varying vec3 vNormal;
 varying vec3 vNormalD;
+varying vec3 vWorldPosition;
+varying vec3 vWorldNormal;
 `;
 
 export const VARYINGS_WIRE_FRAGMENT = `
@@ -29,4 +31,6 @@ varying vec2 vUv;
 varying vec3 vPosition;
 varying vec3 vPositionD;
 varying vec3 vNormalD;
+varying vec3 vWorldPosition;
+varying vec3 vWorldNormal;
 `;
