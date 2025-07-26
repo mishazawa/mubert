94e970c0100c8ef1721c6d8d304a1a1f44250117
asd <macintoshkrug@gmail.com>
2025-07-26 15:26:55 +0300

    rm bs code

--- lib/hooks/useColorGenerator.tsx
+++ lib/hooks/useColorGenerator.tsx
@@ -1,44 +1,29 @@
 import type { RandomGenerator } from "../utils";
 import { formatHex } from "culori";
-import { Poline, positionFunctions } from "poline";
+import { Poline, positionFunctions, type PositionFunction } from "poline";
 import { useMemo } from "react";
 import { Color } from "three";
 
+const POSITION_FUNCTIONS = Object.values(positionFunctions);
 
 const typedTuple = <T extends unknown[]>(...args: T): T => args;
 
 export function useColorGenerator(rand: RandomGenerator, seed: number) {
+  const posFunctions = pickRandomFns(rand);
 
-  const pfns = [
-    positionFunctions["linearPosition"],
-    positionFunctions["exponentialPosition"],
-    positionFunctions["quadraticPosition"],
-    positionFunctions["cubicPosition"],
-    positionFunctions["quarticPosition"],
-    positionFunctions["sinusoidalPosition"],
-    positionFunctions["asinusoidalPosition"],
-    positionFunctions["arcPosition"]
-  ];
-  let pftns_use = [];
-  for (let i = 0; i < 3; i++) {
-    pftns_use.push(pfns[rand.int(0, pfns.length - 1)]);
-  }
-  let hue1 = rand.float(0, 1);
-  let hue2 = hue1 + 0.5;
-  hue1 *= 360;
-  hue2 *= 360;
+  const rv = rand.float(0, 1);
+  const hue1 = rv * 360;
+  const hue2 = (rv + 0.5) * 360;
 
   const palette = useMemo(
     () =>
       new Poline({
         anchorColors: [
           typedTuple(hue1, rand.float(0.0, 0.0), rand.float(0.0, 0.0)),
-          typedTuple(hue1, rand.float(1.0, 1.0), rand.float(1.0, 1.0)),
+          typedTuple(hue2, rand.float(1.0, 1.0), rand.float(1.0, 1.0)),
         ],
         numPoints: 5,
-        positionFunctionX: pftns_use[0],
-        positionFunctionY: pftns_use[1],
-        positionFunctionZ: pftns_use[2],
+        ...posFunctions,
       }),
     [seed]
   );
@@ -53,4 +38,18 @@ export function useColorGenerator(rand: RandomGenerator, seed: number) {
       })
     )
     .map((c) => new Color(c).toArray());
+}
+
+function pickRandomFns(rand: RandomGenerator) {
+  const pftns_use: PositionFunction[] = [];
+  for (let i = 0; i < 3; i++) {
+    pftns_use.push(
+      POSITION_FUNCTIONS[rand.int(0, POSITION_FUNCTIONS.length - 1)]
+    );
+  }
+  return {
+    positionFunctionX: pftns_use[0],
+    positionFunctionY: pftns_use[1],
+    positionFunctionZ: pftns_use[2],
+  };
 }
--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,84 @@ import Canvas from "@lib/main";
 import { useShaderState } from "./controls";
 
 import { useSound } from "./sound";
+import { randomGenerator } from "@lib/utils";
+import { useEffect, useMemo } from "react";
 
 function App() {
   const [data, debug] = useShaderState();
   const fftfns = useSound();
+
+  useAudioTrack(data.uSeed);
+
   return (
     <>
       <Canvas data={data} debug={debug} {...fftfns} />
     </>
   );
+}
+
+function useAudioTrack(seed: number) {
+  const gen = useMemo(() => randomGenerator(seed), []);
+
+  const files = [
+    "Autumn.mp3",
+    "Bazoer.mp3",
+    "Big Beat.mp3",
+    "Bollywood Slow Ballad.mp3",
+    "Braindance.mp3",
+    "Brown Noise.mp3",
+    "Cardio 120.mp3",
+    "Cbdab7ce-1767-471f-b57e-a944ecb5c61f.mp3",
+    "Dreamy.mp3",
+    "Dubstep.mp3",
+    "Dubtechno.mp3",
+    "Electro House.mp3",
+    "Ethnic 108.mp3",
+    "Ethnic.mp3",
+    "Fitness.mp3",
+    "Focus.mp3",
+    "Game.mp3",
+    "Heroic.mp3",
+    "IDM.mp3",
+    "Industrial.mp3",
+    "Malivar.mp3",
+    "Moods.mp3",
+    "Motorik.mp3",
+    "MyOwnWay.mp3",
+    "New Jack Swing.mp3",
+    "Night.mp3",
+    "Nu Disco.mp3",
+    "Om.mp3",
+    "Orchestral.mp3",
+    "Pink Noise.mp3",
+    "Post-Rock.mp3",
+    "Psytrance.mp3",
+    "Pumped.mp3",
+    "Ref_TJID_09293065-dc89-4702-967e-0143ce168914.mp3",
+    "Ref_TJID_0a7ff373-32a5-4c8c-9750-88c338105def.mp3",
+    "Ref_TJID_92f4b544-4b3b-475d-a3dc-a1d2fe6102d1.mp3",
+    "Ref_TJID_be305cc2-6345-4df9-9fcd-c50745646184.mp3",
+    "Run 140.mp3",
+    "Serious.mp3",
+    "Sleep.mp3",
+    "Slow Ballad.mp3",
+    "Spooky.mp3",
+    "Summer.mp3",
+    "Tribal House.mp3",
+    "Vaporwave.mp3",
+    "Vogue.mp3",
+    "Witch House.mp3",
+  ];
+
+  useEffect(() => {
+    const audioelm = document.querySelector("audio");
+
+    if (audioelm) {
+      const file = files[gen.int(0, files.length - 1)];
+      audioelm.setAttribute("src", `/music/${file}`);
+      audioelm.load();
+    }
+  }, [seed]);
 }
 
 export default App;
