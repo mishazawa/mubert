b4cad4c065803e71bee093583c8d67a7c17ec4a6
asd <macintoshkrug@gmail.com>
2025-08-07 22:16:27 +0300

    fix bloom clipping

--- lib/effects/index.tsx
+++ lib/effects/index.tsx
@@ -44,7 +44,7 @@ export function FX() {
       <AudioReactiveGlitch />
 
       <Noise opacity={FX_NOISE_SCALE} />
-      <Bloom mipmapBlur levels={7} intensity={0.5} />
+      <Bloom mipmapBlur levels={7} intensity={1} luminanceThreshold={0.9} />
       <SMAA />
     </EffectComposer>
   );
