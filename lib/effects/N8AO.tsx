5e6dba7aac6343d946fdec93d4f364fc19474270
asd <macintoshkrug@gmail.com>
2025-09-05 16:20:49 +0300

    rm ao

--- lib/effects/N8AO.tsx
+++
@@ -1,8 +0,0 @@
-import { useDebug } from "../hooks/useDebug";
-import { N8AO } from "@react-three/postprocessing";
-
-export function AO() {
-  const ao = useDebug("ao", {});
-
-  return <N8AO {...ao} />;
-}
